c166799596aa97c958bfd890eaa99175e967cc80
Timothy Chu <timchu@google.com>
2023-01-16 12:08:51 -0800

    Added captions, and set timers to enable image clickability (replacing the old method of force-activating it by clicking some unintuitive piece of text)

--- content.js
+++ content.js
@@ -1,13 +1,21 @@
 url = document.URL
 // This is a hack.... the iframe doesn't load or something, and doesn't take up the whole body???
 // Next time, click the button to open the sidepanel as well.
-document.body.innerHTML = "<div> Click me to get started </div>"
-document.body.innerHTML += '<div style="float:left; width:60%"> <iframe id="mainframe" style="height:100vh" width="100%" src="' + url + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>'
-document.body.innerHTML += ' <div style="float:left; width:40%"> <iframe style="height:100vh" width="100%" src="' + chrome.runtime.getURL("normal_popup.html") + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>'
+frames = '<div style="float:left; width:60%"> <iframe id="mainframe" style="height:100vh" width="100%" src="' + url + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>' + ' <div style="float:left; width:40%"> <iframe style="height:100vh" width="100%" src="' + chrome.runtime.getURL("normal_popup.html") + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>'
+document.body.innerHTML = frames
 console.log(document.body.innerHTML)
 
 console.log("Test")
-document.body.addEventListener('click', function(){ clickedBody(); });
+async function clickEnable(t) { 
+  await new Promise(r => setTimeout(r, t));
+  clickedBody();
+}
+// Catch all the links loaded by iframe
+clickEnable(1000);
+clickEnable(2000);
+clickEnable(5000);
+clickEnable(15000);
+//document.body.addEventListener('click', function(){ clickedBody(); });
 function clickedBody(){
   var iframe = document.getElementById('mainframe');
   console.log(iframe.outerHTML);
--- normal_popup.js
+++ normal_popup.js
@@ -1,17 +1,30 @@
+let defaultCaption = "Editable Caption"
+let defaultText = "Click on any image to drop it into this side panel! Images come with Editable Captions"
+document.body.innerHTML = '<h2>' + defaultText + '</h2>'
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-      document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img style="max-height:400px; max-width:100%; height:auto; width: auto" src = "' + request.imageSrc + '"></div>' + document.body.innerHTML;
+      document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img style="max-height:400px; max-width:100%; height:auto; width: auto" src = "' + request.imageSrc + '"><div class="caption" contentEditable>' + defaultCaption + '</div> </div>' + document.body.innerHTML;
       sendResponse({farewell: "goodbye"});
     let close_buttons = document.getElementsByClassName("close-button")
-    for (i = 0; i < close_buttons.length; i++){
+    for (let i = 0; i < close_buttons.length; i++){
       console.log("Close button obtained!");
       close_buttons[i].addEventListener("click", removeImage);
     }
+    let captions = document.getElementsByClassName("caption")
+    for (let i = 0; i < captions.length; i++){
+      console.log("Caption obtained!");
+      captions[i].addEventListener("click", removeText);
+    } 
   }
 );
+function removeText(){
+  if (this.innerHTML== defaultCaption){
+    this.innerHTML=""
+  }
+}
 
 function removeImage() {
   console.log("Removing parentNode");
