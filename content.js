bd0418420054c59f41c7377d0092c2bd43029a52
Timothy Chu <timchu@google.com>
2023-01-16 12:18:12 -0800

    Fixed a bug due to overzealous deleting of console logs (accidentally deleted other text.)

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 url = document.URL
 // This is a hack.... the iframe doesn't load or something, and doesn't take up the whole body???
 // Next time, click the button to open the sidepanel as well.
-frames = '<div style="float:left; width:60%"> <iframe id="mainframe" style="height:100vh" width="100%" src="' + url + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>' + ' <div style="float:left; width:40%"> <iframe style="height:100vh" width="100%" src="' + chrome.runtime.getURL("normal_popup.html") + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>'
+frames = '<div style="float:left; width:60%"> <iframe id="mainframe" style="height:100vh" width="100%" src="' + url + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>' + ' <div style="float:left; width:40%"> <iframe style="height:100vh" width="100%" src="' + chrome.runtime.getURL("side_panel.html") + '" style="-webkit-transform:scale(1);-moz-transform-scale(1);" title="W3Schools Free Online Web Tutorials"></iframe> </div>'
 document.body.innerHTML = frames
 
 async function clickEnable(t) { 
@@ -23,5 +23,5 @@ function clickedBody(){
   }
 }
 function onclick(myImageSrc){
-  (async () => {
-    const response = await chrome.runtime.sendMessage({imageSrc: myImageSrc});
+  (async () => { const response = await chrome.runtime.sendMessage({imageSrc: myImageSrc}); })();
+}
