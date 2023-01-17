e35492eb8c240a915d7519ef2db636cf73d38cc5
Timothy Chu <timchu@google.com>
2023-01-16 17:47:00 -0800

    Got scroll working, but it jumps up to the top due to the way we're re-rendering the whole document HTML every time an image is put into the side. Fix the scroll later by using document.body.appendNode to append objects rather than re-writing the HTML the whole time

--- side_panel.js
+++ side_panel.js
@@ -1,26 +1,33 @@
 let defaultCaption = "(Optional Caption)"
 let defaultText = "Click on any image to drop it into this side panel! Images come with Editable Captions"
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  async function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
     console.log(JSON.stringify(request));
     if (request.imageSrc){
         console.log("Adding image");
-        document.body.innerHTML += '<div class=image-container><br><div class="close-button">X</div><img style="max-height:400px; max-width:100%; height:auto; width: auto" src = "' + request.imageSrc + '"><div class="caption" contentEditable>' + defaultCaption + '</div> </div>';// + document.body.innerHTML;
+        document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img style="max-height:300px; max-width:100%; height:auto; width: auto" src = "' + request.imageSrc + '"><div class="caption" contentEditable>' + defaultCaption + '</div> </div>' + document.body.innerHTML;
         sendResponse({farewell: "got Image Source"});
     }
     if (request.text) {
         console.log("Adding text");
-        document.body.innerHTML += '<div class=text-container><br><div class="close-button">X</div><div style="font-size: 16px; max-height:400px; max-width:100%; height:auto; width: auto">' + request.text + '</div></div>';// + document.body.innerHTML;
+        document.body.innerHTML = '<div class=text-container><br><div class="close-button">X</div><div style="font-size: 16px; max-height:400px; max-width:100%; height:auto; width: auto">' + request.text + '</div></div>' + document.body.innerHTML;
         sendResponse({farewell: "got Text"});
 
     }
-/* Aborted attempt to make the sidepanel scroll. I think the problem is that the document HTML doesn't render before I try to scroll down*/
-    
-    console.log(document.body.scrollHeight);
-    scrollTo(0, document.body.scrollHeight);
+/* Injected an await so that the image can render before scroll.*/
+/* Change code to append node to document rather than re-writing the whole thing via document.body.innerHTML += , to avoid the scroll jumping to the top. I think this eliminates everything and re-renders everything, which causes the scroll to jump up (as the window gets small when everything's not-rendered) and then back down. */
+
+/* Uncomment when activating scroll
+    async function scroll(){ 
+      await new Promise(r => setTimeout(r, 100));
+      console.log(document.body.scrollHeight);
+      scrollTo(0, document.body.scrollHeight);
+    }
+    scroll();
+*/
 
     // Add Close button functionality. This is needed since setting onClick manually in HTML is banned (why is it banned?)
     let close_buttons = document.getElementsByClassName("close-button")
