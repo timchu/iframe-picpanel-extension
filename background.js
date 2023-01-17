9a5f17aeea47307d91c269fc016fb9ea60a429d9
Timothy Chu <timchu@google.com>
2023-01-17 10:57:41 -0800

    Got URL for tab in background.js on clicking extension

--- background.js
+++ background.js
@@ -1,4 +1,11 @@
 console.log("Background loaded")
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tabInfo) => {
     console.log("Clicked!")
+    console.log(await getCurrentTab());
 });
+async function getCurrentTabUrl() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
+}
+
